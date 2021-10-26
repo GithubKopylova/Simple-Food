785f840070e4493f7c441043b14071ee6e562082
unknown <kopilova.ania@yandex.ru>
2021-10-27 01:37:58 +0300

    auto_awesome

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ const replace       = require('gulp-replace');
 const cheerio       = require('gulp-cheerio');
 
 const svgSprites = () => {
-  return src (['app/images/icons/**.svg'])
+  return src (['docs/images/icons/**.svg'])
   .pipe(cheerio({
     run:function($) {
       $('[fill]').removeAttr('fill');
@@ -29,27 +29,27 @@ const svgSprites = () => {
       }
     },
   }))
-  .pipe(dest('app/images/'))
+  .pipe(dest('docs/images/'))
 }
 
 function browsersync() {
   browserSync.init({
     server: {
-      baseDir:'app/'
+      baseDir:'docs/'
     },
     notify: false
   })
 }
 
 function styles() {
-  return src('app/scss/style.scss')
+  return src('docs/scss/style.scss')
   .pipe(scss({outputStyle:'compressed'}))
   .pipe(concat('style.min.css'))
   .pipe(autoprefixer({
     overrideBrowserslist: ['last 10 versions'],
     grid: true
   }))
-  .pipe(dest('app/css'))
+  .pipe(dest('docs/css'))
   .pipe(browserSync.stream())
 }
 
@@ -58,16 +58,16 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js',
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/slick-carousel/slick/slick.js',
-    'app/js/main.js'
+    'docs/js/main.js'
   ])
   .pipe(concat('main.min.js'))
   .pipe(uglify())
-  .pipe(dest('app/js'))
+  .pipe(dest('docs/js'))
   .pipe(browserSync.stream())
 }
 
 function images(){
-  return src('app/images/**/*.*')
+  return src('docs/images/**/*.*')
   .pipe(imagemin([
     imagemin.gifsicle({interlaced: true}),
     imagemin.mozjpeg({quality: 75, progressive: true}),
@@ -84,10 +84,10 @@ function images(){
 
 function build () {
   return src([
-    'app/**/*.html',
-    'app/css/style.min.css',
-    'app/js/main.min.js'
-  ], {base:'app'})
+    'docs/**/*.html',
+    'docs/css/style.min.css',
+    'docs/js/main.min.js'
+  ], {base:'docs'})
   .pipe(dest('dist'))
 }
 
@@ -96,10 +96,10 @@ function cleanDist() {
 }
 
 function watching() {
-  watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-  watch(['app/**/*.html']).on('change', browserSync.reload);
-  watch(['app/images/icons/**.svg'], svgSprites);
+  watch(['docs/scss/**/*.scss'], styles);
+  watch(['docs/js/**/*.js', '!docs/js/main.min.js'], scripts);
+  watch(['docs/**/*.html']).on('change', browserSync.reload);
+  watch(['docs/images/icons/**.svg'], svgSprites);
 }
 
 exports.svgSprites   = svgSprites;
